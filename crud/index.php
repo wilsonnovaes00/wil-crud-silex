32490c607e55695f517a38a1383d996fcfe50a1a
wilsonnovaes00 <wilsonnovaes00@hotmail.com>
2016-04-14 22:41:49 -0300

    update silex 1.0

--- crud/index.php
+++ crud/index.php
@@ -1,3 +1,25 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+
+$app = new Silex\Application(); 
+
+$app['debug'] = true;
+
+$app->get('/', function () use ($app) {
+
+    return new Response(file_get_contents('../pg/templates/template.html'), 200);
+});
+
+
+
+$app->run();
+
+
 
