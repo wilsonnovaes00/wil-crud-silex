7fe1dbc00d50bd5dc2292f3085a273a87af4a524
Wilson Filho <wilsonnovaes00@hotmail.com>
2016-04-21 15:57:06 -0300

    Add comentaries in aplication and Register Twig Provider

--- crud/index.php
+++ crud/index.php
@@ -3,14 +3,22 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
+#Add Autoload
 require_once __DIR__.'/../vendor/autoload.php';
 
 
+#Starting Silex Application
 $app = new Silex\Application();
-$app->get('/', function () use ($app) {
 
-    return new Response(file_get_contents('../template/template.php'), 200);
+#Registering a Twig Provider
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/template',
+));
+
+
+#Routes
+$app->get('/', function () use ($app) {
+    
 });
 
 
